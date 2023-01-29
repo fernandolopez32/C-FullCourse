9b59f3e893464386041b7b6b5afa7e7398d41534
Fernando Lopez <flopez00032@gmail.com>
2023-01-29 12:20:00 -0600

    cout my name 10 times

--- main.cpp
+++ main.cpp
@@ -5,9 +5,17 @@
 // main function starting point of the c++ program
 // code is executed in order top to bottom 
 int main(){
-    std:: cout << "Hello World!" << std:: endl;
-    std:: cout << "Number 1";
-    std:: cout << "Number 2";
 
+    // endl is used to go to the next line in the terminal 
+            // std:: cout << "Hello World!" << std:: endl;
+            // std:: cout << "Number 1" << std:: endl;
+            // std:: cout << "Number 2" << std:: endl;
+    // printing out my name 10 times 
+    for(int i = 0; i < 10; i++){
+        std:: cout << "Fernando Lopez" << std:: endl;
+    }
+    
+
+    // sometimes used to see if the whole program ran correctly
    return 0; 
 }

35652c4b328c57761f5398c348422fbbd6cc2c0a
Fernando Lopez <flopez00032@gmail.com>
2023-02-03 09:19:06 -0600

    added comment description

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
+/* 
+If you are reading you need to know the comment structure--->
+
+    //things commented at this indentation are notes to myself or word problems that I decided to solve
+
+            //any comment at this indentatino is the solution to the given problem
+        
+
+*/
+
+
+
+
+
+
 #include <iostream>
 // loading libs that we can use for example function to print out hello world
+#include <string>
+
+
+using namespace std;
+
 double addingDecimals(double first, double second){
         return first + second;
     }
@@ -8,18 +28,18 @@ double addingDecimals(double first, double second){
 // code is executed in order top to bottom 
 int main(){
 
-    // endl is used to go to the next line in the terminal 
+// endl is used to go to the next line in the terminal 
             // std:: cout << "Hello World!" << std:: endl;
             // std:: cout << "Number 1" << std:: endl;
             // std:: cout << "Number 2" << std:: endl;
-    // printing out my name 10 times 
+// printing out my name 10 times 
             // for(int i = 0; i < 10; i++){
             //                     // this is how you put a variable in a terminal log
             //     std:: cout << "Fernando Lopez" << std:: endl;
             // }
-    // functions are writen out the same as java 
+// functions are writen out the same as java 
     
-    //store the sum in a variable and print out that variables vallue
+//store the sum in a variable and print out that variables vallue
 
             // double sum = addingDecimals(3.14, 3.43);
 
@@ -27,9 +47,22 @@ int main(){
 
             // std::cout << "The sum of these decimals is : " << addingDecimals(2.3,4.1)<< std::endl;
 
-    // this is how you write an array 
-    int myArray[5] = {1,2,3,4,5};
-    std::cout << "this is array at index number two: " << myArray[2]<< std::endl;
+// this is how you write an array 
+            // int myArray[5] = {1,2,3,4,5};
+            // std::cout << "this is array at index number two: " << myArray[2]<< std::endl;
+
+
+// write a application in the terminal to have a conversation with.
+    string userResp = "";
+
+    // do 
+    // {
+    //     std:: cout << "hey looser" << std::endl;
+    // }
+    // while (/* condition */)
+    // {
+    //     /* code */
+    // };
 
 
     // sometimes used to see if the whole program ran correctly

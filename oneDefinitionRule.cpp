6bdedc339551419cbe6923fc26987d634700ce47
Fernando Lopez <flopez00032@gmail.com>
2023-02-21 10:05:13 -0600

    my first object oriented code in c++

--- oneDefinitionRule.cpp
+++ oneDefinitionRule.cpp
@@ -1,25 +1,36 @@
 #include <iostream> 
-
+#include "person.h"
 using namespace std;
 
 
 // Variable : Declaration and definition 
 
-double weight{};
+// double weight{};
 
-double add(double a, double b);
+// double add(double a, double b){
+//     return a+b;
+// };
+
+// struct Point 
+// {
+//     double m_x{0}; 
+//     double m_y{0}; 
+// };
 
 
 int main(){
 
+
     // cout << weight << endl;
 
-    double result = add(10,20);
-    cout << "Result : " << result << endl;
+    // double result = add(10,20);
+    // cout << "Result : " << result << endl;
 
+    // Point p1; 
+    // cout<< "p1.x : " << p1.m_x << " , p1.y : " << p1.m_y << endl;
+
+    Person p1 ("John Snow" , 35);
+    p1.print_info();
 
     return 0; 
-}
-double add(double a, double b){
-    return a+b;
 }
